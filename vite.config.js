4e0e1fdc9e682489b23cd13b9b40de7b12228c03
Tech Tac <ahmad.tech.tac@gmail.com>
2023-05-28 15:06:51 +0300

    Tweaked vite file

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/sass/app.scss",
-                "resources/js/app.js",
-                "bootstrap",
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
